0a878e43bcd0c06fdb12f71931833a19d7977993
Vlad Kosarev <vlad@webmustflow.com>
2013-05-23 22:36:39 -0400

    Single threaded dumb file server
    In a few lines of code can serve files from running folder
    Will try to serve index.html if file with the name is not available
    Just fooling around

---
+++ StaticPageServer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace StaticPageServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int port = 80;
+                TcpListener server = new TcpListener(IPAddress.Any, port);
+                server.Start();
+
+                byte[] bytes = new byte[1024];
+
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    NetworkStream stream = client.GetStream();
+
+                    int i = stream.Read(bytes, 0, bytes.Length);
+
+                    var data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    var req = data.Split(' ');
+                    if (req.Length > 3)
+                    {
+                        var path = req[1];
+                        var filePath = "." + path.Replace('/', '\\');
+                        Console.WriteLine(filePath);
+                        if (File.Exists(filePath))
+                        {
+                            var content = File.ReadAllText(filePath);
+                            stream.Write(System.Text.Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n" + content), 0, content.Length + 19);
+                        }
+                        else if (Directory.Exists(filePath))
+                        {
+                            if (File.Exists("index.html"))
+                            {
+                                var content = File.ReadAllText(filePath + "\\index.html");
+                                stream.Write(System.Text.Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n" + content), 0, content.Length + 19);
+                            }
+                            else
+                            {
+                                stream.Write(System.Text.Encoding.ASCII.GetBytes("HTTP/1.1 404 Not Found"), 0, 22);
+                            }
+                        }
+                        else
+                        {
+                            stream.Write(System.Text.Encoding.ASCII.GetBytes("HTTP/1.1 404 Not Found"), 0, 22);
+                        }
+                    }
+                    stream.Close();
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+    }
+}
---
+++ StaticPageServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StaticPageServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StaticPageServer")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1397354c-2e4d-4a00-a106-c0836644e044")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
